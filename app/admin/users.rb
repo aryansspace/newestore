09dc40ecf139eb2d50642e0653f390afc2475f8a
Aryan <aryan8965sharma@gmail.com>
2019-11-20 09:37:59 -0600

    updated stuff

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -12,7 +12,7 @@ ActiveAdmin.register User do
   # permit_params do
   #   permitted = [:email, :encrypted_password, :reset_password_token, :reset_password_sent_at, :remember_created_at, :name]
   #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted 
+  #   permitted changed users.rb
   # end
   
 end
